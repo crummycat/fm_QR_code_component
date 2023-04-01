a09bc85324fd7ae858ed08e63a0ada2969703d4c
crummycat <chloe.bouvier@me.com>
2023-04-01 19:55:21 +0100

    Added preload and js to prevent flash of unstyled content on page load

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// $(window).load(function () {
+//   $("body").removeClass("preload");
+// });
+window.addEventListener("load", (event) => {
+  var element = document.getElementsByClassName("preload");
+  element[0].classList.remove("preload");
+});
